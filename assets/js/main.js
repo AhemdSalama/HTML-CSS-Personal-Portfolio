6ce4a989c8e27c451fb146326924382fb4a5f422
AhmedSalama <ahmedsalamafcis@gmail.com>
2023-04-11 15:23:48 +0200

    Finished nav section in mobile version CSS and JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,34 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close");  
+
+/*===== MENU SHOW =====*/
+/* Validate if Constant exist */
+if (navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu');
+    });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constent exist */
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
 
+const linkAction = ()=>{
+    const navMenu = document.getElementById("nav-menu");
+    /* when we click on any nav link we remove show-menu */
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=> n.addEventListener('click',linkAction))
 
 /*=============== SWIPER PROJECTS ===============*/
 
